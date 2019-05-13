221a5da440ac89dd758ec9367f6e6eaee4431f48
calvincaudill <50593450+calvincaudill@users.noreply.github.com>
2019-05-13 20:54:03 +0200

    React app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    
+    <p>whatever</p>
+    
+
+
+
+
+    
+  );
+}
+
+export default App;
